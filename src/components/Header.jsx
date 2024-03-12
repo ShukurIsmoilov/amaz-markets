d9ccf748566622052e5024975315d0823c2dccc8
Shukur Ismoilov <shukur_007_96@mail.ru>
2024-03-12 22:13:22 +0500

    Refactor header component to support multiple languages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,124 @@
-import logo from '../assets/logo.png';
-import './Header.css';
+import { useState, useEffect } from "react";
+import logo from "../assets/logo.png";
+import "./Header.css";
 
 const Header = () => {
 
-  const headerLinks = [
-    { to: "buy_account", text: "Buy Account", active: false },
-    { to: "sell_account", text: "Sell Account", active: false },
-    { to: "services", text: "Services", active: true },
-    { to: "blog", text: "Blog", active: false },
-    { to: "search", text: "Search", active: false },
+  const headerLinksFromStorageEn = [
+    {
+      id: "hl1",
+      to: "buy_account",
+      text: "Buy Account",
+      active: true,
+    },
+    {
+      id: "hl2",
+      to: "sell_account",
+      text: "Sell Account",
+      active: false,
+    },
+    {
+      id: "hl3",
+      to: "services",
+      text: "Services",
+      active: false,
+    },
+    {
+      id: "hl4",
+      to: "blog",
+      text: "Blog",
+      active: false,
+    },
+    {
+      id: "hl5",
+      to: "search",
+      text: "Search",
+      active: false
+    },
   ]
 
-  // const [activeLink, setActiveLink] = useState(0);
+  const headerLinksFromStorageRu = [
+    { id: "hl1", text: "Купить Аккаунт" },
+    { id: "hl2", text: "Продать Аккаунт" },
+    { id: "hl3", text: "Услуги" },
+    { id: "hl4", text: "Блог" },
+    { id: "hl5", text: "Поиск" },
+  ]
+
+  const [headerLinks, setHeaderLinks] = useState(headerLinksFromStorageEn);
+  const [activeLinkId, setActiveLinkId] = useState(headerLinks[0].id);
+  const [selectedLang, setSelectedLang] = useState("EN");
+
+
+  useEffect(() => {
+    const newHeaderLinks = headerLinks.map((item) => {
+      return {
+        ...item,
+        active: false,
+      };
+    });
+    newHeaderLinks.find((item) => item.id === activeLinkId).active = true;
+    setHeaderLinks(newHeaderLinks);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [activeLinkId]);
+
+  useEffect(() => {
+    if (selectedLang === "EN") {
+      const newHeaderLinks = headerLinks.map((item) => {
+        return {
+          ...item,
+          text: headerLinksFromStorageEn.find((link) => link.id === item.id).text
+        };
+      });
+      setHeaderLinks(newHeaderLinks);
+    } else {
+      const newHeaderLinks = headerLinks.map((item) => {
+        return {
+          ...item,
+          text: headerLinksFromStorageRu.find((link) => link.id === item.id).text
+        };
+      });
+      setHeaderLinks(newHeaderLinks);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedLang])
 
   return (
     <header className="header">
       <nav>
-        <div className='container' style={{
-          display: "flex",
-          justifyContent: "space-between",
-          padding: "10px",
-        }}>
-          <img src={logo} alt="" />
-
-          <ul style={{
-            listStyle: "none",
-            display: "flex",
-            gap: "40px",
-          }}>
-            {headerLinks.map((link, index) => {
+        <div className="container">
+          <a href="/">
+            <img src={logo} alt="home" />
+          </a>
+          <ul className="link_list">
+            {headerLinks.map((link) => {
               return (
-                <li style={{
-                  color: "#EB6108",
-                  display: "flex",
-                  gap: "5px",
-                  flexDirection: "column",
-                  alignItems: "center",
-                
-                }} key={index}>
-                  <a className={link.active ? "active" : ""} href={link.to}>{link.text}</a>
-                  <span style={{
-                    width: "5px",
-                    height: "5px",
-                    borderRadius: "50%",
-                    backgroundColor: link.active ? "#EB6108" : "transparent",
-                  }}></span>
+                <li className="link_list_item" key={link.id}>
+                  <a
+                    className={link.active ? "active_link" : ""}
+                    onClick={() => setActiveLinkId(link.id)}
+                  >
+                    {link.text}
+                  </a>
                 </li>
               )
             })}
-            <li style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "10px",
-            }}>
-              <button className="lang_btn active_lang">En</button>
-              <button className="lang_btn">Ru</button></li>
+            <li className="link_list_item_for_lang">
+              <button
+                className={`${selectedLang === "EN" ? "active_lang" : ""}`}
+                onClick={() => setSelectedLang("EN")}
+              >
+                En
+              </button>
+              <button
+                className={` ${selectedLang === "RU" ? "active_lang" : ""}`}
+                onClick={() => setSelectedLang("RU")}
+              >
+                Ru
+              </button>
+            </li>
           </ul>
         </div>
       </nav>
